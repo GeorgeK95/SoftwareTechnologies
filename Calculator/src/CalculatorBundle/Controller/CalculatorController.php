2b78fd5fff86644729b462602ec2c93c25ac18f3
GeorgeK95 <george_it@abv.bg>
2017-03-12 13:26:02 +0200

    Making calculator to work.

--- Calculator/src/CalculatorBundle/Controller/CalculatorController.php
+++ Calculator/src/CalculatorBundle/Controller/CalculatorController.php
@@ -21,6 +21,58 @@ class CalculatorController extends Controller
      */
     public function index(Request $request)
     {
-        return $this->render('calculator/index.html.twig');
+        $calc = new Calculator();
+
+        $form = $this->createForm(CalculatorType::class, $calc);
+        $form->handleRequest($request);
+
+        if ($form->isValid() && $form->isSubmitted()) {
+            $firstNum = $calc->getLeftOperand();
+            $secondNum = $calc->getRightOperand();
+            $operator = $calc->getOperator();
+            $result = 0;
+
+            switch ($operator) {
+                case "+":
+                    $result = $firstNum + $secondNum;
+                    break;
+                case "-":
+                    $result = $firstNum - $secondNum;
+                    break;
+                case "*":
+                    $result = $firstNum * $secondNum;
+                    break;
+                case "/":
+                    $result = $firstNum / $secondNum;
+                    break;
+                case "^":
+                    $result = pow($firstNum, $secondNum);
+                    break;
+                case "||":
+                    if (($firstNum == 0 || $firstNum == 1) && ($secondNum == 0 || $secondNum == 1)) {
+                        if ($firstNum == 0 && $secondNum == 0) {
+                            $result = 0;
+                        } else
+                            $result = 1;
+                    } else
+                        $result = "II";//invalid input
+
+                    break;
+                case "&&":
+                    if (($firstNum == 0 || $firstNum == 1) && ($secondNum == 0 || $secondNum == 1)) {
+                        if ($firstNum == 1 && $secondNum == 1) {
+                            $result = 1;
+                        } else
+                            $result = 0;
+                    } else
+                        $result = "II";//invalid input
+
+                    break;
+            }
+
+            return $this->render('calculator/index.html.twig', ['result' => $result, 'calculator' => $calc, 'form' => $form->createView()]);
+        }
+
+        return $this->render('calculator/index.html.twig', ['form' => $form->createView()]);
     }
 }
