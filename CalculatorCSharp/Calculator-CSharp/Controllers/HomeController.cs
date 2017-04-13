39f851f102e814f84e5e8c5e9f920b7ef845a517
GeorgeK95 <george_it@abv.bg>
2017-04-13 18:36:26 +0300

    Up C# asp.net mvc Calculator.

---
+++ CalculatorCSharp/Calculator-CSharp/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Calculator_CSharp.Models;
+using System;
+using System.Web.Mvc;
+
+namespace Calculator_CSharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(Calculator calculator)
+        {
+            return View(calculator);
+        }
+
+        [HttpPost]
+        public ActionResult Calculate(Calculator calculator)
+        {
+            calculator.result = CalculateResult(calculator);
+            return RedirectToAction("Index", calculator);
+        }
+        public double CalculateResult(Calculator calculator)
+        {
+            double res = 0;
+
+            switch (calculator.operand)
+            {
+                case "+":
+                    res = calculator.leftOperand + calculator.rightOperand;
+                    break;
+                case "-":
+                    res = calculator.leftOperand - calculator.rightOperand;
+                    break;
+                case "*":
+                    res = calculator.leftOperand * calculator.rightOperand;
+                    break;
+                case "/":
+                    res = calculator.leftOperand / calculator.rightOperand;
+                    break;
+                case "^":
+                    res = Math.Pow(calculator.leftOperand, calculator.rightOperand);
+                    break;
+                case "||":
+                    if ((calculator.leftOperand == 0 || calculator.leftOperand == 1) && (calculator.rightOperand == 0 || calculator.rightOperand == 1))
+                    {
+                        if (calculator.leftOperand == 0 && calculator.rightOperand == 0)
+                        {
+                            res = 0;
+                        }
+                        else
+                            res = 1;
+                    }
+                    else
+                        res = -1;//invalid input
+                    break;
+                case "&&":
+                    if ((calculator.leftOperand == 0 || calculator.leftOperand == 1) && (calculator.rightOperand == 0 || calculator.rightOperand == 1))
+                    {
+                        if (calculator.leftOperand == 1 && calculator.rightOperand == 1)
+                        {
+                            res = 1;
+                        }
+                        else
+                            res = 0;
+                    }
+                    else
+                        res = -1;//invalid input
+                    break;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ CalculatorCSharp/Calculator-CSharp/Models/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Calculator_CSharp.Models
+{
+    public class Calculator
+    {
+        public double leftOperand { get; set; }
+        public double rightOperand { get; set; }
+        public string operand { get; set; }
+        public double result { get; set; }
+        public Calculator()
+        {
+            this.result = 0;
+        }
+
+       
+    }
+}
